8bcc6a263bdadc2fdf07b619eecd8bfaa2e2efd7
AnnBerinceva <berinceva92@bk.ru>
2022-12-13 18:09:28 +0500

    HW_RES_PROG

---
+++ HW_RES_PROGRAM/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[10] {"Hello", "2", "world", ")))", "1234", "156", "-2", "Russia", "-8", "abc"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int a = 0; a < array1.Length; a++)
+    {
+    if(array1[a].Length <= 3)
+        {
+        array2[count] = array1[a];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int a = 0; a < array.Length; a++)
+    {
+        Console.Write($"{array[a]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
